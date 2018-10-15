8abddbd17c0af74c9bc23dd627b308f23420a20e
tejprakash <tejprakash1590@gmail.com>
2018-10-15 17:58:24 -0500

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id:10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
- var decoded = jwt.verify(token,'123abc');
- console.log(decoded);
+// bcrypt.genSalt(10, (err,salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$O32Dwjx2Q53lynZI7u6XdeEHDKDP8cqNwNbQ3Yux6OG6DXBgn0Y1S';
+
+bcrypt.compare(passwor , hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id:10
+// };
+//
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+//  var decoded = jwt.verify(token,'123abc');
+//  console.log(decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
--- server/server.js
+++ server/server.js
@@ -94,13 +94,11 @@ app.post('/users', (req,res) => {
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-    res.header('X-auth', token).send(user);
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   });
 });
-
-
 
 app.get('/users/me',authenticate,(req,res) => {
   res.send(req.user);
